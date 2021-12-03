8c0ed8c2261808f402561f7a2a67a1518f384efd
emmanuel <emmanueliyare7@gmail.com>
2021-12-03 20:57:19 +0100

    filter updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import './App.css';
 import UserList from './UserList';
-import SearchBox from './SearchBox';
-import { useState } from 'react';
 
 function App() {
-  const [searchField, setSearchField] = useState('')
-
-const onSearchChange = (event) => {
-  setSearchField(event.target.value)
-}
-
   return (
     <div class="App">
-      <SearchBox 
-      searchChange={onSearchChange}
-       />
       <UserList />
     </div>
   );
--- src/UserList.js
+++ src/UserList.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
+import SearchBox from './SearchBox';
 import React, { useState, useEffect } from 'react'
 import './style.css'
 
 function UserList() {
-  const [listOfUser, setListOfUser] = useState([])
+  // const [listOfUser, setListOfUser] = useState([])
+  //  const [searchField, setSearchField] = useState('')
+   const [listOfUser, setListOfUser] = useState({
+    users: [],
+    searchField: '',
+  });
+
+  const filteredUsers = listOfUser.users.filter((user) => {
+    return user.name.toLowerCase().includes(listOfUser.searchField.toLowerCase());
+  });
+
+  const onSearchChange = (e) => {
+    setListOfUser({ searchField: e.target.value });
+  };
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users')
@@ -16,16 +30,23 @@ function UserList() {
     })
   }, [])
 
+  // const onSearchChange = (event) => {
+  // setSearchField(event.target.value)
+  // }
+
   return (
     <div >
+     <SearchBox 
+      searchChange={onSearchChange}
+       />
       <ul className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
         {
-          listOfUser.map(listOfUser => (
+          users.map(users => (
             <div>
-            <h3>{listOfUser.name}</h3>
-            <h4>{listOfUser.username}</h4>
-            <h5>{listOfUser.email}</h5>
-            <h6>{listOfUser.address.street}</h6>
+            <h3>{users.name}</h3>
+            <h4>{users.username}</h4>
+            <h5>{users.email}</h5>
+            <h6>{users.address.street}</h6>
             <hr />
             </div>
           ))
